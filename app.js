9c745774d7a3bbba6da032287948a285937217e5
StudentUserDiego03 <dcardenascastane@student.alamo.edu>
2024-03-20 15:07:40 -0500

    created functions

---
+++ app.js
@@ -0,0 +1,23 @@
+function myFunction(){
+    const ageInput= document.getElementById("age");
+    if(!ageInput.checkValidity()){
+        document.getElementById("message").innerHTML=ageInput.validationMessage;
+    }
+    else{
+        document.getElementById("message").innerHTML="your approved";
+    }
+
+}
+
+function register(){
+    const FullnameInput = document.getElementById("fullName").value;
+    localStorage.setItem("name", FullnameInput);
+    document.getElementById("account").innerHTML=localStorage.getItem("name");
+}
+
+getText ("text.txt");
+async function getText (file){
+    let textMessage = await fetch(file);
+    let Text = await textMessage.text();
+    document.getElementById("demo").innerHTML=Text;
+}
